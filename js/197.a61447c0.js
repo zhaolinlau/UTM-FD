a81bc6330f80c1f32eb4197e3e5bbb35b6c8ed0e
Zhaolin Lau <zhaolinlau@gmail.com>
2022-05-19 21:47:15 +0800

    added foundation info, partners, about

--- js/197.a61447c0.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkutmfd"]=self["webpackChunkutmfd"]||[]).push([[197],{9197:function(t,e,n){n.r(e),n.d(e,{default:function(){return v}});var l=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("v-container",{staticClass:"fill-height"},[n("v-row",[n("v-col",[n("h1",{staticClass:"text-center display-1"},[t._v("仍在开发中...")])])],1)],1)},s=[],u=n(1001),r=n(3453),a=n.n(r),c=n(2102),i=n(9846),f=n(2877),o={},h=(0,u.Z)(o,l,s,!1,null,null,null),v=h.exports;a()(h,{VCol:c.Z,VContainer:i.Z,VRow:f.Z})}}]);
-//# sourceMappingURL=197.a61447c0.js.map
