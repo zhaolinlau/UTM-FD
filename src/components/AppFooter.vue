aba33e45b6e2a9d8972226efbb3a76781bee74f5
Zhaolin Lau <68846168+zhaolinlau@users.noreply.github.com>
2022-12-10 12:27:16 +0800

    update license
    Signed-off-by: Zhaolin Lau <68846168+zhaolinlau@users.noreply.github.com>

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,15 @@
 <template>
-  <v-footer class="mt-16">
-      <v-col class="text-center">
-        Copyright &copy; {{ new Date().getFullYear() }} <strong>工大Foundation & Diploma升学辅导站</strong> - All Rights Reserved
-      </v-col>
-    </v-footer>
+	<v-footer class="mt-16">
+		<v-col class="text-center">
+			Source code is licensed under
+			<a href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank"
+				>GNU GPLv3</a
+			>. Website content is licensed under
+			<a
+				href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+				target="_blank"
+				>CC BY-NC-SA 4.0</a
+			>.
+		</v-col>
+	</v-footer>
 </template>
