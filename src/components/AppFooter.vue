2936dc26902eeddf0423ae82620fbeacb242b271
Zhaolin Lau <zhaolinlau@gmail.com>
2023-03-02 13:42:27 +0800

    change footer link color to red

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,9 +1,9 @@
 <template>
 	<v-footer class="mt-16">
 		<v-col class="text-center">
-			<a href="https://github.com/Luetify/UTM-FD" target="_blank">Source code</a> licensed
-			<a href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank">GNU GPLv3</a>. Website content licensed
-			<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a>.
+			<a href="https://github.com/Luetify/UTM-FD" target="_blank" class="text-red">Source code</a> licensed
+			<a href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank" class="text-red">GNU GPLv3</a>. Website content licensed
+			<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" class="text-red">CC BY-NC-SA 4.0</a>.
 		</v-col>
 	</v-footer>
 </template>
