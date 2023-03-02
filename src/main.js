3350684cfa4d89f94cabbb9f4e57b7ef95eea8dc
Zhaolin Lau <zhaolinlau@gmail.com>
2023-03-02 13:37:06 +0800

    add animate.css

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import "@mdi/font/css/materialdesignicons.css";
+import "animate.css/animate.css"
 
 const app = createApp(App);
 
