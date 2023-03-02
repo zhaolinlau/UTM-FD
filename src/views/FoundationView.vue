c27089b95950411ccb8ffa34f876814ebfbaa4ee
Zhaolin Lau <zhaolinlau@gmail.com>
2023-03-02 13:45:53 +0800

    add live update link

--- src/views/FoundationView.vue
+++ src/views/FoundationView.vue
@@ -1,3 +1,19 @@
 <template>
-	
+	<v-row class="text-center">
+		<v-col>
+			<p class="text-h5">
+				<v-icon color="red" class="animate__animated animate__fadeInLeft animate__infinite">
+					mdi-hand-pointing-right</v-icon>
+				实时更新
+				<v-icon color="red" class="animate__animated animate__fadeInRight animate__infinite">
+					mdi-hand-pointing-left</v-icon>
+			</p>
+			<p>
+				Foundation回忆录实时更新版本
+				<a href="https://docs.google.com/document/d/1JIv_cj_trtyMBduYT572OWnSf8xK8tJxdOg2oG0bQmc/edit?usp=sharing"
+					target="_blank" class="text-red">点击这里</a>
+				追踪
+			</p>
+		</v-col>
+	</v-row>
 </template>
