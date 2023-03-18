878ef034bba7a8452882acffbbc623fef5eca423
Zhaolin Lau <zhaolinlau@gmail.com>
2023-03-18 15:02:21 +0800

    add menu list to nav drawer

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -6,7 +6,14 @@ const drawer = ref(false)
 
 <template>
 	<v-navigation-drawer v-model="drawer" temporary>
+		<v-list>
+			<v-list-item to="/">首页</v-list-item>
+			<v-list-item to="/foundation">Foundation</v-list-item>
+			<v-list-item to="/diploma">Diploma</v-list-item>
+			<v-list-item to="/about">关于</v-list-item>
+		</v-list>
 	</v-navigation-drawer>
+
 	<v-app-bar>
 		<v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
 		<v-app-bar-title>工大Foundation & Diploma升学辅导站</v-app-bar-title>
