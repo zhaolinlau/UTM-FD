ee24292a2401c57dff5983d3e95560d8d9730921
Zhaolin Lau <zhaolinlau@gmail.com>
2023-03-19 15:06:03 +0800

    add live update section for diploma

--- src/views/DiplomaView.vue
+++ src/views/DiplomaView.vue
@@ -1,3 +1,21 @@
 <template>
-	
+	<v-row id="live">
+		<v-col>
+			<p class="text-h5 text-center">
+				<v-icon color="red" class="animate__animated animate__fadeInLeft animate__infinite"
+					icon="mdi-hand-pointing-right"></v-icon>
+				实时更新
+				<v-icon color="red" class="animate__animated animate__fadeInRight animate__infinite"
+					icon="mdi-hand-pointing-left"></v-icon>
+			</p>
+			<span class="text-center">
+				<p>
+					Diploma回忆录实时更新版本
+					<a href="https://docs.google.com/document/d/1adcmMMb_TGeoGya83xHyUAMP3Ccwq3XYlr83uI0Hq4c/edit?usp=sharing"
+						target="_blank" class="text-red">点击这里</a>
+					追踪
+				</p>
+			</span>
+		</v-col>
+	</v-row>
 </template>
