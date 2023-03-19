0842234ea9db8532177a6d0ef80f64f73e2eff2b
Zhaolin Lau <zhaolinlau@gmail.com>
2023-03-19 22:24:41 +0800

    add about us

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+	<v-row>
+		<v-col cols="12">
+			<p class="text-center text-h5">关于</p>
+		</v-col>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+		<v-col cols="12">
+			<p class="text-h6">
+				工大Diploma & Foundation升学辅导站是由UTM Foundation以及UTM
+				Diploma前辈创立的一个非盈利并且非属于任何组织的小组。本小组成立主要是为了能提供新生们相关的课程资讯以及帮助正在困惑的新生并协助解决疑惑。
+			</p>
+		</v-col>
+	</v-row>
+	<div class="d-flex justify-center">
+		<img src="@/assets/thank.svg">
+	</div>
+</template>
