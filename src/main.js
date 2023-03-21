c90265c254134b223179090f0bab36cd10e5129f
Zhaolin Lau <zhaolinlau@gmail.com>
2023-03-22 01:30:38 +0800

    remove mdi and add fontawesome

--- src/main.js
+++ src/main.js
@@ -3,16 +3,24 @@ import App from "./App.vue";
 import router from "./router";
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
+import { aliases, fa } from "vuetify/iconsets/fa";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import "@mdi/font/css/materialdesignicons.css";
-import "animate.css/animate.css"
+import "@fortawesome/fontawesome-free/css/all.css";
+import "animate.css/animate.css";
 
 const app = createApp(App);
 
 const vuetify = createVuetify({
 	components,
 	directives,
+	icons: {
+		defaultSet: "fa",
+		aliases,
+		sets: {
+			fa,
+		},
+	},
 });
 
 app.use(router).use(vuetify);
