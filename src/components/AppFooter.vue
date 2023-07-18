90471b86719fd42f8cfac06853c62a9ffecf8e63
Zhaolin Lau <zhaolinlau@gmail.com>
2023-07-19 02:16:38 +0800

    add .bg-transparent to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-footer class="mt-16">
+	<v-footer class="mt-16 bg-transparent">
 		<v-col class="text-center">
 			<a href="https://github.com/Luetify/UTM-FD" target="_blank" class="text-red">Source code</a> licensed
 			<a href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank" class="text-red">GNU GPLv3</a>. Website content licensed
