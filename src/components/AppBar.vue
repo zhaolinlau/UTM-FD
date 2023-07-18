49eaefd5212258fcd88a1ef2388e8f624b4bc728
Zhaolin Lau <zhaolinlau@gmail.com>
2023-07-19 01:53:02 +0800

    add light & dark theme toggle button

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { useTheme } from 'vuetify'
+const toggleTheme = () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+const theme = useTheme()
+</script>
+
 <template>
 	<v-app-bar title="工大Foundation & Diploma升学辅导站" scroll-behavior="hide">
 		<v-tabs color="red" class="hidden-sm-and-down">
@@ -6,5 +12,7 @@
 			<v-tab to="/diploma">Diploma</v-tab>
 			<v-tab to="/about">关于</v-tab>
 		</v-tabs>
+		<v-btn @click="toggleTheme"
+			:icon="theme.global.current.value.dark ? 'fa-regular fa-moon' : 'fa-regular fa-sun'"></v-btn>
 	</v-app-bar>
 </template>
