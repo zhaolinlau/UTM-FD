7de4b654695d26efeda0454ec267d937710ee426
Zhaolin Lau <zhaolinlau@gmail.com>
2023-07-19 02:03:53 +0800

    modify group session

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,17 +13,17 @@ import AppCarousel from '@/components/AppCarousel.vue'
 
 	<v-row justify="center">
 		<v-col cols="12" lg="4" md="4" xl="4">
-			<v-card title="UTM Foundation 2023/2024 Telegram 群组">
+			<v-card title="UTM Foundation 2022/2023 Telegram 群组">
 				<v-card-actions>
-					<v-btn prepend-icon="fa-brands fa-telegram" color="blue" block active rounded>未开放</v-btn>
+					<v-btn prepend-icon="fa-brands fa-telegram" color="blue" block active rounded>已关闭</v-btn>
 				</v-card-actions>
 			</v-card>
 		</v-col>
 
 		<v-col cols="12" lg="4" md="4" xl="4">
-			<v-card title="UTM Diploma 2023/2024 WhatsApp 群组">
+			<v-card title="UTM Diploma 2022/2023 WhatsApp 群组">
 				<v-card-actions>
-					<v-btn prepend-icon="fa-brands fa-whatsapp" color="green" block active rounded>未开放</v-btn>
+					<v-btn prepend-icon="fa-brands fa-whatsapp" color="green" block active rounded>已关闭</v-btn>
 				</v-card-actions>
 			</v-card>
 		</v-col>
